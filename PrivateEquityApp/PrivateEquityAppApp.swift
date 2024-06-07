c6db8c099efb1796ee3eca12dd48ee11929f61ed
Steven Zhang <stevezzhang@gmail.com>
2024-06-07 15:29:59 -0700

    Initial Commit

---
+++ PrivateEquityApp/PrivateEquityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateEquityAppApp.swift
+//  PrivateEquityApp
+//
+//  Created by Steven Zhang on 2024-06-07.
+//
+
+import SwiftUI
+
+@main
+struct PrivateEquityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
